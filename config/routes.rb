3844f706eb02d30327ae0556829b9fe78cce4545
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-05 07:25:58 +0000

    8-finished

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
